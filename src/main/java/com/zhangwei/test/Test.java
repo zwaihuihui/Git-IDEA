3d40f63cb6028c2aeacdc2adcd9de918826aa7f5
zhangwei <3226645389@qq.com>
2022-03-27 16:53:09 +0800

    first commit by zhangwei

---
+++ src/main/java/com/zhangwei/test/Test.java
@@ -0,0 +1,13 @@
+package com.zhangwei.test;
+
+/**
+ * @author zw
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/27 16:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git,hello 1");
+    }
+}
